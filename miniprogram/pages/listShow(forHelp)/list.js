0317770c79ebdd4aa2251251431f9ec78cadec4a
WincyYu <13521790809@163.com>
2019-05-05 19:23:39 +0800

    add the data to list

--- miniprogram/pages/listShow(forHelp)/list.js
+++ miniprogram/pages/listShow(forHelp)/list.js
@@ -14,5 +14,102 @@ Page({
       fail: function (res) { },
       complete: function (res) { },
     })
+  },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list1: []
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  //读取数据
+  onLoad: function (options) {
+    var _this = this;
+    const db = wx.cloud.database({
+      env: 'hyb-1ab04c'
+    })
+    db.collection('test').where({
+    })
+      .get({
+        success: res => {
+          this.setData({
+            list1: res.data
+          })
+        }
+      })
+    this.setData({
+      type: options.type,
+    });
+    console.log(options.type);
+  },
+  onTapToSuccess: function () {
+    wx.navigateTo({
+      url: '/pages/test1/test1',
+      success: function () {
+        console.log("jump success")
+      },
+      fail: function () {
+        console.log("jump failed")
+      },
+      complete: function () {
+        console.log("jump complete")
+      }
+    })
+  },
+
+
+
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   }
 })
